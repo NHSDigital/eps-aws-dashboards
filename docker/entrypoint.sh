d3c513e2cd2d53bad7ae8f1f73709733bfbfc124
Kris Szlapa <kris.szlapa1@nhs.net>
2024-10-09 15:02:41 +0000

    Add ShellCheck directive for sourcing the file

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-source /home/cdkuser/.asdf/asdf.sh
+export ASDF_DIR="$HOME/.asdf"
+
+# shellcheck source=/home/vscode/.asdf/lib/asdf.sh
+source "$ASDF_DIR/lib/asdf.sh"
 
 epsZoneId=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "eps-route53-resources:EPS-ZoneID") | .Value' | grep -o '[^:]*$')
 epsDomain=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "eps-route53-resources:EPS-domain") | .Value' | grep -o '[^:]*$')
