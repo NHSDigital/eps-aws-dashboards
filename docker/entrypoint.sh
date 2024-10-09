ac9b0ae461438575ba082a2fc41881d1de5ff2d3
Kris Szlapa <kris.szlapa1@nhs.net>
2024-10-09 15:08:24 +0000

    Fix the shellcheck issue

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-export ASDF_DIR="$HOME/.asdf"
-
-# shellcheck source=/home/vscode/.asdf/lib/asdf.sh
-source "$ASDF_DIR/lib/asdf.sh"
+# shellcheck source=/dev/null
+source /home/cdkuser/.asdf/asdf.sh
 
 epsZoneId=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "eps-route53-resources:EPS-ZoneID") | .Value' | grep -o '[^:]*$')
 epsDomain=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "eps-route53-resources:EPS-domain") | .Value' | grep -o '[^:]*$')
