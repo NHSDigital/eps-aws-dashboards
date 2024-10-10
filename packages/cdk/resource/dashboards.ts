752e166b71a59e976e7c9ea98c192777471a1c0c
Kris Szlapa <kris.szlapa1@nhs.net>
2024-10-10 11:15:45 +0000

    Use class-based constructs approach for dashboards

--- packages/cdk/resource/dashboards.ts
+++ packages/cdk/resource/dashboards.ts
@@ -5,43 +5,50 @@ import {createApiGatewayWidget} from "./apiGatewayMetrics"
 import {createLambdaMetricWidget} from "./lambdaMetrics"
 import {createStepFunctionWidget} from "./stepFunctionMetrics"
 
-export const createDashboard = (scope: Construct) => {
-  const dashboard = new cw.Dashboard(scope, "Dashboards", {
-    defaultInterval: Duration.days(7),
-    variables: [
-      new cw.DashboardVariable({
-        id: "region2",
-        type: cw.VariableType.PATTERN,
-        label: "RegionPattern",
-        inputType: cw.VariableInputType.INPUT,
-        value: "eu-west-2",
-        defaultValue: cw.DefaultValue.value("eu-west-2"),
-        visible: true
-      })
-    ]
-  })
+export class Dashboards extends Construct {
+  public readonly dashboardArn: string // Expose the dashboard ARN as a public property
 
-  // Adding widgets in a structured way (flattened)
-  dashboard.addWidgets(
-    // First Row
-    createApiGatewayWidget("4XXError"),
-    createApiGatewayWidget("5XXError"),
+  public constructor(scope: Construct, id: string) {
+    super(scope, id)
 
-    // Second Row
-    createApiGatewayWidget("Latency"),
-    createApiGatewayWidget("IntegrationLatency"),
+    // Create the dashboard
+    const dashboard = new cw.Dashboard(scope, "Dashboards", {
+      defaultInterval: Duration.days(7),
+      variables: [
+        new cw.DashboardVariable({
+          id: "region2",
+          type: cw.VariableType.PATTERN,
+          label: "RegionPattern",
+          inputType: cw.VariableInputType.INPUT,
+          value: "eu-west-2",
+          defaultValue: cw.DefaultValue.value("eu-west-2"),
+          visible: true
+        })
+      ]
+    })
 
-    // Third Row
-    createApiGatewayWidget("Count"),
-    createStepFunctionWidget("PfP Step Functions"),
+    // Adding widgets in a structured way
+    dashboard.addWidgets(
+      // First Row
+      createApiGatewayWidget("4XXError"),
+      createApiGatewayWidget("5XXError"),
 
-    // Widgets are stacked vertically in a single column
-    createLambdaMetricWidget("Errors"),
-    createLambdaMetricWidget("Duration"),
-    createLambdaMetricWidget("Invocations"),
-    createLambdaMetricWidget("PostRuntimeExtensionsDuration"),
-    createLambdaMetricWidget("Throttles")
-  )
+      // Second Row
+      createApiGatewayWidget("Latency"),
+      createApiGatewayWidget("IntegrationLatency"),
 
-  return dashboard
+      // Third Row
+      createApiGatewayWidget("Count"),
+      createStepFunctionWidget("PfP Step Functions"),
+
+      // Widgets are stacked vertically in a single column
+      createLambdaMetricWidget("Errors"),
+      createLambdaMetricWidget("Duration"),
+      createLambdaMetricWidget("Invocations"),
+      createLambdaMetricWidget("PostRuntimeExtensionsDuration"),
+      createLambdaMetricWidget("Throttles")
+    )
+
+    this.dashboardArn = dashboard.dashboardArn // Store the ARN of the dashboard
+  }
 }
--- packages/cdk/stacks/dashboardsStack.ts
+++ packages/cdk/stacks/dashboardsStack.ts
@@ -1,20 +1,24 @@
+// packages/cdk/stacks/dashboardsStack.ts
 import * as cdk from "aws-cdk-lib"
-import {createDashboard} from "../resource/dashboards"
+import {Dashboards} from "../resource/dashboards"
 
 /**
  * EPS AWS Dashboards
  */
 
 export class DashboardsStack extends cdk.Stack {
+  public readonly dashboardArn: string // Expose the ARN as a public property
+
   public constructor(scope: cdk.App, id: string, props: cdk.StackProps) {
     super(scope, id, props)
 
-    // Create the dashboard and its widgets
-    const dashboard = createDashboard(this)
+    // Create the dashboards construct with a unique ID
+    const dashboards = new Dashboards(this, "EpsAwsDashboards")
 
-    // Add an output for the dashboard
+    // Outputs
+    this.dashboardArn = dashboards.dashboardArn // Store the dashboard ARN
     new cdk.CfnOutput(this, "DashboardURL", {
-      value: dashboard.dashboardArn,
+      value: this.dashboardArn,
       description: "The ARN of the CloudWatch Dashboard"
     })
   }
