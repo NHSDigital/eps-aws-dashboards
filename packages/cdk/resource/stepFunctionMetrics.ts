5d22bb7f58558e4b36fc63e7bb8034a9bc427b2b
Kris Szlapa <kris.szlapa1@nhs.net>
2024-10-15 15:27:52 +0000

    Pass stack value to step function metrics

--- packages/cdk/resource/stepFunctionMetrics.ts
+++ packages/cdk/resource/stepFunctionMetrics.ts
@@ -7,8 +7,8 @@ type MetricConfig = {
 }
 
 const generateStepFunctionMetrics = (
-  stack: Stack,
-  stateMachineName: string
+  stateMachineName: string,
+  stack: Stack
 ): Array<MetricConfig> => {
   const metricNames = [
     "ExecutionsStarted",
@@ -26,12 +26,12 @@ const generateStepFunctionMetrics = (
   }))
 }
 
-const createStepFunctionMetric = (config: MetricConfig, region: string) => {
+const createStepFunctionMetric = (config: MetricConfig, stack: Stack) => {
   return new cw.Metric({
     namespace: "AWS/States",
     metricName: config.metricName,
     dimensionsMap: config.dimensions,
-    region: region
+    region: stack.region
   })
 }
 
@@ -39,16 +39,15 @@ export const createStepFunctionWidget = (
   title: string,
   stack: Stack,
   stateMachineName: string,
-  region: string = "eu-west-2",
   period: number = 300,
   height: number = 6,
   width: number = 8
 ) => {
   return new cw.GraphWidget({
     title: title,
-    region: region,
-    left: generateStepFunctionMetrics(stack, stateMachineName).map((metricConfig) =>
-      createStepFunctionMetric(metricConfig, region)
+    region: stack.region,
+    left: generateStepFunctionMetrics(stateMachineName, stack).map((metricConfig) =>
+      createStepFunctionMetric(metricConfig, stack)
     ),
     view: cw.GraphWidgetView.TIME_SERIES,
     stacked: false,
